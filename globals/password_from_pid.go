63e207c551f781a6581593cb7760100a05c35564
Daniel López Guimaraes <danielectra@outlook.com>
2023-10-29 21:26:08 +0000

    Simplify some code
    This is already handled by the account server.

--- globals/password_from_pid.go
+++ globals/password_from_pid.go
@@ -12,16 +12,10 @@ import (
 func PasswordFromPID(pid uint32) (string, uint32) {
 	ctx := metadata.NewOutgoingContext(context.Background(), GRPCAccountCommonMetadata)
 
-	response, err := GRPCAccountClient.GetNEXData(ctx, &pb_account.GetNEXDataRequest{Pid: pid})
+	response, err := GRPCAccountClient.GetNEXPassword(ctx, &pb_account.GetNEXPasswordRequest{Pid: pid})
 	if err != nil {
 		globals.Logger.Error(err.Error())
 		return "", nex.Errors.RendezVous.InvalidUsername
-	}
-
-	// * We only allow tester accounts for now
-	if response.AccessLevel < 1 {
-		globals.Logger.Errorf("PID %d is not a tester!", response.Pid)
-		return "", nex.Errors.RendezVous.AccountDisabled
 	}
 
 	return response.Password, 0
